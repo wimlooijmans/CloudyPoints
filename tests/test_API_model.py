b1816c3908c571f558c4a49c1c6c8580d37ef9dd
Wim Looijmans <looijmanswim@gmail.com>
2025-04-26 15:45:06 +0200

    Change function name to test_request_home() in test_API_model.py

--- tests/test_API_model.py
+++ tests/test_API_model.py
@@ -1,9 +1,7 @@
 import pytest
 from ..tests.conftest import client
 
-print("test_API_model.py --- imporst done")
-
-def test_request_example(client):
+def test_request_home(client):
     response = client.get("/")
     print("response", response)
     assert response.status_code == 200
